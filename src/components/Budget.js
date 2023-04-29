0d123d7734e339fec39616b07d1347f9aa9958d0
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2023-04-28 19:04:22 -0600

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,34 +2,33 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget, setBudget, Currency } = useContext(AppContext);
-    const [inputBudget, setInputBudget] = useState('');
+  const { setBudget, currency } = useContext(AppContext);
+  const [inputBudget, setInputBudget] = useState(2000);
+  
+  const handleInputChange = (e) => {
+    setInputBudget(e.target.value);
+  };
 
-    const handleInputChange = (e) => {
-        setInputBudget(e.target.value);
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputBudget > 20000) {
+      alert("The value cannot exceed {currency}20000");
+      return;
+    }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    setBudget(parseInt(inputBudget));
+    setInputBudget('');
+  };
 
-        if (inputBudget > 20000) {
-            alert("The value cannot exceed £20000");
-            return;
-        }
-
-        setBudget(parseInt(inputBudget));
-        setInputBudget('');
-    };
-
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: </span>
-            <form onSubmit={handleSubmit}>
-             {Currency}<input type="number" value={inputBudget} onChange={handleInputChange} step="10" />
-                <button type="submit">Update Budget</button>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      Budget: {currency}{inputBudget}
+      <form onSubmit={handleSubmit}>
+        <input type="number" value={inputBudget} onChange={handleInputChange} />
+        <button>Update Budget</button>
+      </form>
+    </div>
+  );
 };
 
 export default Budget
